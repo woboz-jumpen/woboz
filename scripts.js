05cb1837e3bea97b7efca9081c913c6f94f87d99
Alex <aleczanderlukasha0@gmail.com>
2023-12-22 11:34:31 -0500

    big changes B)

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function redirectToSteam() {
+    window.open('https://steamcommunity.com/id/torcida_berlin', '_blank_')
+}
+function redirectToDiscord() {
+    window.open('discord://-/users/1085210641238855831', '_blank')
+}
+function redirectToInstagram() {
+    window.open('https://www.instagram.com/schlafer_jedentag/', '_blank')
+}
